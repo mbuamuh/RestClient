b72504ba48993d7345876aefe0575a309a8b5961
Unice Gwe Mbuamuh <umbuamuh@redhat.com>
2012-10-15 09:29:16 +0100

    jar with dependencies

--- src/main/java/org/redhat/MainPaymentClient.java
+++ src/main/java/org/redhat/MainPaymentClient.java
@@ -1,39 +1,40 @@
 package org.redhat;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.redhat.model.Order;
 
 public class MainPaymentClient {
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 		
+		File inputFile = new File(args[0]);	
+//		File inputFile = new File("/tmp/Order.xml");
+		Order order = getOrderFromXml(inputFile );
 		PaymentClient paymentClient = new PaymentClient();
-		// To do: create an xml or csv file to read all the orders from and instead of declaring them manually in the main class
-		Order order1 = new Order();
-		order1.setChildName("Thomas");
-		order1.setId(1);
-		order1.setMenu("menu2");
-		order1.setParentName("Chris");
-		order1.setPrice(30.0);
-		
-		Order order2 = new Order();
-		order2.setChildName("Peter");
-		order2.setId(2);
-		order2.setMenu("menu3");
-		order2.setParentName("Fred");
-		order2.setPrice(50.5);
-		
-		Order order3 = new Order();
-		order3.setChildName("Steve");
-		order3.setId(3);
-		order3.setParentName("Jackson");
-		order3.setMenu("menu4");
-		order3.setPrice(23.5);
-		
-		paymentClient.postPaymentOrder(order1);
-		paymentClient.postPaymentOrder(order2);
-		paymentClient.postPaymentOrder(order3);
-		
+		paymentClient.postPaymentOrder(order);
+	}
+
+	private static Order getOrderFromXml(File inputFile) {
+	Order order = null;
 	
+	try {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Order.class);
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+       order = (Order) unmarshaller.unmarshal(inputFile);
+	
+	} catch (JAXBException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+		return order;
 	}
 
 }
